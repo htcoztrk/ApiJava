00ff296b1ede5688b0578187461e3f46d001768c
hatice <hatice.ozturk@testinium.com>
2024-10-18 22:52:52 +0300

    initial commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+S
+#src/main/java/org/example/Main.java,0/b/0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+J
+src/test/java/ApiTest.java,f/6/f6c2df86b25f265d629171059728af9d10989970
+K
+src/test/java/ApiTest2.java,1/7/171b60a3c75cefac38af3191671d85633cf5252f
---
+++ src/test/java/ApiTest2.java
@@ -0,0 +1,24 @@
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApiTest2 {
+
+    @Test
+    public void testGetRequest() {
+        // Bir örnek GET isteği
+        HttpResponse<JsonNode> response = Unirest.get("https://jsonplaceholder.typicode.com/posts/1")
+                .asJson();
+
+        // Yanıtı kontrol et
+        assertEquals(200, response.getStatus());
+        JsonNode body = response.getBody();
+        System.out.println(body.toString());
+
+        // Yanıtın id alanını kontrol et
+        assertEquals(1, body.getObject().getInt("id"));
+    }
+}
