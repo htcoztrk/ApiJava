a2c72eaf9abc21b9de1fa4a1519b5797bc6f9d66
hatice <hatice.ozturk@testinium.com>
2024-10-18 22:56:31 +0300

    initial commit

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -21,4 +21,18 @@ public class ApiTest {
         // Yanıtın id alanını kontrol et
         assertEquals(1, body.getObject().getInt("id"));
     }
+    @Test
+    public void testGetRequest2() {
+        // Bir örnek GET isteği
+        HttpResponse<JsonNode> response = Unirest.get("https://jsonplaceholder.typicode.com/posts/1")
+                .asJson();
+
+        // Yanıtı kontrol et
+        assertEquals(200, response.getStatus());
+        JsonNode body = response.getBody();
+        System.out.println(body.toString());
+
+        // Yanıtın id alanını kontrol et
+        assertEquals(1, body.getObject().getInt("id"));
+    }
 }
--- src/test/java/ApiTest2.java
+++ src/test/java/ApiTest2.java
@@ -8,7 +8,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ApiTest2 {
 
     @Test
-    public void testGetRequest() {
+    public void testGetRequest3() {
+        // Bir örnek GET isteği
+        HttpResponse<JsonNode> response = Unirest.get("https://jsonplaceholder.typicode.com/posts/1")
+                .asJson();
+
+        // Yanıtı kontrol et
+        assertEquals(200, response.getStatus());
+        JsonNode body = response.getBody();
+        System.out.println(body.toString());
+
+        // Yanıtın id alanını kontrol et
+        assertEquals(1, body.getObject().getInt("id"));
+    }
+    @Test
+    public void testGetRequest4() {
         // Bir örnek GET isteği
         HttpResponse<JsonNode> response = Unirest.get("https://jsonplaceholder.typicode.com/posts/1")
                 .asJson();
